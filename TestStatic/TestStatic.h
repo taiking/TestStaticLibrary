02027f4ff98d041a69db13f05c74fc941bca47f9
chocovayashi <c.tsujiyan727@gmail.com>
2020-09-10 14:48:16 +0900

    Initial Commit

---
+++ TestStatic/TestStatic.h
@@ -0,0 +1,13 @@
+//
+//  TestStatic.h
+//  TestStatic
+//
+//  Created by 辻林　大揮 on 2020/09/10.
+//  Copyright © 2020 VOYAGE GROUP, INC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestStatic : NSObject
+
+@end
